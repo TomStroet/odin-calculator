e6f5f5684f0d7312a357f58be23a9b9c1d676cbf
Tom <tomstroet@gmail.com>
2026-03-06 22:09:37 +0100

    logic refactor

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selectors
 const display = document.querySelector('.display')
 const clearBtn = document.querySelector('#clear')
 const backspaceBtn = document.querySelector('#backspace')
@@ -5,17 +6,106 @@ const numpad = document.querySelectorAll('.numpad button:not(#plusMinus)')
 const operators = document.querySelectorAll('.operators button');
 const equalsBtn = document.querySelector('#equals')
 
-let operator = ''
-let inputString = ''
-let values = []
 
-let operations = {
-    'add':      (acc, num) =>  acc + num,
-    'subtract': (acc, num) =>  acc - num,
-    'multiply': (acc, num) =>  acc * num,
-    'divide':   (acc, num) =>  acc / num,
+// State
+const state = {
+    display: '',
+    operator: '',
+    leftNum: null,
+    rightNum: null,
+    result: null
 }
 
+
+// Utility functions
+const operations = {
+    add:      (a, b) =>  a + b,
+    subtract: (a, b) =>  a - b,
+    multiply: (a, b) =>  a * b,
+    divide:   (a, b) =>  a / b,
+}
+
+
+// App logic
+function pressNumberButton(state, buttonString) {
+    if (state.result) {
+        state.display = '';
+        state.result = null;
+    }
+    state.display += buttonString;
+    logState()
+}
+
+function pressOperatorButton(state, operator) {
+    if (!state.leftNum && state.display) {
+        state.leftNum = parseFloat(state.display);
+        state.display = '';
+    }
+    if (state.leftNum && state.operator && state.display) {
+        operate(state, operations)
+    }
+    if (state.result) {
+        state.leftNum = state.result;
+    }
+    if (state.leftNum) {
+        state.operator = operator;
+    }
+    logState()
+}
+
+function operate(state, operations) {
+    if (state.leftNum && state.operator && state.display) {
+        state.rightNum = parseFloat(state.display);
+        logState()
+        state.result = operations[state.operator](state.leftNum, state.rightNum)
+        state.leftNum = null
+        state.rightNum = null
+        state.operator = ''
+        logState()
+        state.display = state.result
+    }
+    logState()
+}
+
+
+// UI/render functions
+function render() {
+    display.textContent = state.display;
+}
+
+function logState() {
+    console.log(state)
+}
+
+
+// Event handlers
+numpad.forEach((button) => 
+    button.addEventListener('click', () => {
+        pressNumberButton(state, button.textContent);
+        render();
+    })
+);
+
+operators.forEach((button) =>
+    button.addEventListener('click', (e) => {
+        pressOperatorButton(state, e.target.id);
+        if (state.result) {
+            render();
+        }
+    })
+);
+
+equalsBtn.addEventListener('click', () => {
+    operate(state, operations)
+    render()
+})
+
+clearBtn.addEventListener('click', () => init())
+
+backspaceBtn.addEventListener('click', () => pressBackspaceButton())
+
+
+// Initialization
 function init() {
     values = []
     operator = ''
@@ -23,29 +113,7 @@ function init() {
     refreshDisplay(inputString)
 }
 
-function log() {
-    console.log(`inputString ${inputString}`)
-    console.log(`values: ${values}`)
-    console.log(`operator ${operator}`)
-    console.log(`Display: ${display.textContent}`)
-}
 
-function refreshDisplay(string) {
-    display.textContent = string;
-}
-
-function pressNumberButton(buttonString) {
-    inputString += buttonString;
-    refreshDisplay(inputString)
-     
-    let i = 0;
-    if (operator != '') {
-        i = 1;
-    }
-    values[i] = parseFloat(inputString)
-
-    log()
-}
 // NOT WORKING YET.
 // function pressBackspaceButton() {
 //     inputString = inputString.substring(0,inputString.length -1);
@@ -53,41 +121,4 @@ function pressNumberButton(buttonString) {
 //     log()
 // }
 
-function pressOperatorButton(operatorString) {
-    // If there are two inputs, then do the operation before setting the operator and clearing the inputstring.
-    if (values.length == 2) {
-        operate()
-    }
-    operator = operatorString
-    inputString = ''
-    log()
-}
-
-function operate() {
-    if (values.length == 2 && operator != '') {
-        result = values.reduce(operations[operator])
-        init()
-        values = [result]
-        refreshDisplay(result)
-    }
-    log()
-}
-
-
-numpad.forEach((button) => 
-    button.addEventListener('click', () => {
-        pressNumberButton(button.textContent);
-    })
-);
-
-operators.forEach((button) =>
-    button.addEventListener('click', (e) => {
-        pressOperatorButton(e.target.id);
-    })
-);
-
-equalsBtn.addEventListener('click', () => operate())
-
-clearBtn.addEventListener('click', () => init())
-
-backspaceBtn.addEventListener('click', () => pressBackspaceButton())
+logState()
